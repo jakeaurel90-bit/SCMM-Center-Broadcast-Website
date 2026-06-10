49f031f80590818712baf9c6964dab149d4ed378
jakeaurel90-bit <jakeaurel90@gmail.com>
2026-06-10 16:30:28 +0800

    Secure ALLOWED_HOSTS and set debug to false

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-only-for-dev')
 
-DEBUG = True
+DEBUG = False
 
 # Updated ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for Railway
 ALLOWED_HOSTS = ['*']
