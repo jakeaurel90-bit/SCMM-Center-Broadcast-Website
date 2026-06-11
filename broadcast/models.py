66698bece440ecfab65b72c5c00a32a28d9085ec
jakeaurel90-bit <jakeaurel90@gmail.com>
2026-06-11 15:50:53 +0800

    Force Post model to use Cloudinary storage

--- broadcast/models.py
+++ broadcast/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from cloudinary_storage.storage import MediaCloudStorage
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    # The 'upload_to' value acts as a folder name in your Cloudinary account
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True) 
+    
+    # By adding 'storage=MediaCloudStorage()', we force Django 
+    # to send this image directly to Cloudinary, not the local server.
+    image = models.ImageField(
+        upload_to='post_images/', 
+        storage=MediaCloudStorage(), 
+        blank=True, 
+        null=True
+    )
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
